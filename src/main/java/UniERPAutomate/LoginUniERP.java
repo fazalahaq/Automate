152288177f56ccd32ee6fe8d95e063aa91ece61b
unicode <unicode@USPL-329>
2024-10-07 16:56:17 +0530

    Selenium Push

---
+++ src/main/java/UniERPAutomate/LoginUniERP.java
@@ -0,0 +1,13 @@
+package UniERPAutomate;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginUniERP {
+
+	public static void main(String[] args) {
+		// Launch Browser
+		ChromeDriver driver = new ChromeDriver();
+
+	}
+
+}
