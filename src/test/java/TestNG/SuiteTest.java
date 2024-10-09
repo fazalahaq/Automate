d898dea74ed8b64ea7a29b082c84317f554b21ac
unicode <unicode@USPL-329>
2024-10-09 19:12:03 +0530

    new files

---
+++ src/test/java/TestNG/SuiteTest.java
@@ -0,0 +1,13 @@
+package TestNG;			
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+public class SuiteTest {
+
+  @Test
+  public void bftestTest() {
+   // throw new RuntimeException("Test not implemented");
+	  System.out.print("Helloe");
+  }
+
+}
---
+++ src/test/java/TestNG/demo.java
@@ -0,0 +1,21 @@
+package TestNG;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+
+public class demo {
+	
+	@Test
+	void text() {
+		System.out.print("This is Demo Class");
+	}
+	@AfterClass
+	void afazal() {
+		System.out.print("This is Demo After class");
+	}
+
+}
